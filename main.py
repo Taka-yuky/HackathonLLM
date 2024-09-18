6b39ebfa71300f47df67b3e7741f75728a6c4efd
Taka-yuky <kmusic0509@gmail.com>
2024-09-18 23:11:18 +0900

    初コミット！

---
+++ main.py
@@ -0,0 +1,27 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_aws.chat_models import ChatBedrock
+
+def chat_bedrock(message):
+    chat = ChatBedrock(
+        model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0",
+        model_kwargs={"temperature":1.0},
+        region_name="us-east-1",
+    )
+
+    messages = [
+        SystemMessage("ここにはシステム用のメッセージが入ります。"),
+        HumanMessage(message)
+    ]
+
+    content=""
+
+    try:
+        response = chat.invoke(messages)
+        content = response.content
+    except Exception as e:
+        print(e)
+
+    return content
+
+if __name__ == "__main__":
+    chat_bedrock("はじめまして！よろしくね。")
